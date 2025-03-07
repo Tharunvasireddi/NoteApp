edc57592c24eb78899e8d33d1af54ea56d6508bf
Tharunvasireddi <tharunvasireddy73@gmail.com>
2025-03-08 03:18:21 +0530

    notes app is comitted

---
+++ src/notesapp.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    let addbtn = document.getElementById("addbtn");
+    let notesContainer = document.getElementById("notescontainer");
+    let notes = document.querySelectorAll(".input");
+
+        function showNotes(){
+            notesContainer.innerHTML = localStorage.getItem("notes");
+        }
+        showNotes();
+    function updateStorage(){
+        localStorage.setItem("notes",notesContainer.innerHTML);
+    }
+    addbtn.addEventListener("click",()=>{
+        let paper = document.createElement("p");
+        paper.className = "input";
+        paper.setAttribute("contenteditable","true");
+        let img = document.createElement("img");
+        img.className = "trash";
+        img.src ="./src/assets/trash.png";
+        notesContainer.appendChild(paper).appendChild(img);
+        });
+    notesContainer.addEventListener("click",(e)=>{
+        if(e.target.tagName === "IMG"){
+            e.target.parentElement.remove();
+            updateStorage();
+        }
+        else if(e.target.tagName === "P"){
+            notes = document.querySelectorAll(".input");
+            notes.forEach(nt =>{
+                nt.onKeyUp = function(){
+                    updateStorage();
+                }
+            })
+        }
+    })
+})
